449537f1b404199bb27c7509ffb7ddecc63a0599
kamal <c.lamunes@gmail.com>
2023-06-07 20:46:47 +0700

    remove file app.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var message = "Hello coding";
-let msg1 ="Hallo";
-const msg2 = "Coding";
+var message = "Hello Coding";
+let msg1 = "Hallo";
+const msg2 = "Coding"
 
-console.log(message)
+console.log(msg2)
