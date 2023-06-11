f6f1f5eee685d32745e72cec2d7e257c5fc600f4
kamal <c.lamunes@gmail.com>
2023-06-11 16:41:31 +0700

    update js

---
+++ function.js
@@ -0,0 +1,16 @@
+function jalan1() {
+    console.log("jalan satu")
+}
+
+const Jalan2 = function () {
+    console.log("jalan dua")
+}
+
+jalan1()
+Jalan2()
+
+const jalan3 = ()=>{
+    console.log("jalan tiga")
+}
+
+jalan3()
---
+++ tugas3.js
@@ -0,0 +1,14 @@
+function sepeda(){
+    const motorlistrik = "jenis motor"
+    return motorlistrik;
+}
+const motorlistrik = sepeda();
+console.log(motorlistrik);
+
+function tampilkandata(x,y,z){
+
+    console.log(x,y,z);
+
+}
+
+tampilkandata("saya","baik","dan sehat");
